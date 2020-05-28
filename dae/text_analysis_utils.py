948ea6c8d947dff2adf39982a2252d3f77c6fa65
sf-wa-326 <sf_wa_326@outlook.com>
2020-05-28 18:25:45 -0400

    align incomplete

--- dae/text_analysis_utils.py
+++ dae/text_analysis_utils.py
@@ -81,7 +81,7 @@ def basic_check_diff(diffs_result_list):
 
 
 def get_diff_score(
-    hypothesis: str, reference: str
+    hypothesis: str, reference: str, gen_spacy_tokens: list,
 ) -> Tuple[List[Tuple[str, str]], Dict[str, float]]:
     """
     Return the op code and text as a tuple.
@@ -100,6 +100,42 @@ def get_diff_score(
 
         return text[start:end], end
 
+
+    def list_find(big_list, sub_list, end_idx):
+        """
+        analogus method of find (which was for finding substring) for sub list
+        :param big_list:
+        :param sub_list:
+        :return:
+        """
+
+        big_list = big_list[end_idx:]
+        len_big, len_sub = len(big_list[end_idx:]), len(sub_list)
+        for i in range(len_big):
+            spacy_sub_str_accidental_white_space = rouge._preprocess_summary_as_a_whole(''.join(big_list[i:i + len_sub]))[0].replace('-', '')
+            spacy_sub_str = spacy_sub_str_accidental_white_space.replace(' ', '')
+            tokens_sub_str = ''.join(sub_list)
+            # if rouge._preprocess_summary_as_a_whole(' '.join(big_list[i:i + len_sub])) == ' '.join(sub_list):
+            if spacy_sub_str == tokens_sub_str:
+                return i + end_idx
+            else:
+                return None
+
+    def get_sublist(gen_spacy_tokens: list, start: int, tokens) -> Tuple[str, int, int, int]:
+        """
+        Get the original substring from the text for the given sequence of tokens
+        """
+        if tokens == ['where', 'jun', 'seo', 'was']:
+            print('note')
+
+        end = list_find(gen_spacy_tokens, tokens, start)
+        if end == None:
+            print('end = none')
+        end += 1
+        return ' '.join(gen_spacy_tokens[start:end]), end, start, end
+
+    gen_spacy_tokens = [token.lower() for token in gen_spacy_tokens]
+
     score = 0.0
     reference_index = 0
     hypothesis_index = 0
@@ -114,12 +150,17 @@ def get_diff_score(
     diffs_result_list = []
     for tag, alo, ahi, blo, bhi in matcher.get_opcodes():
         if tag == "equal":
-            diff_text, hypothesis_index = get_substring(
-                hypothesis, hypothesis_lc, hypothesis_index, hypothesis_tokens[alo:ahi]
-            )
+
             diff_text, reference_index = get_substring(
                 reference, reference_lc, reference_index, reference_tokens[blo:bhi]
             )
+
+            diff_text, hypothesis_index, start, end = get_sublist(
+                gen_spacy_tokens,
+                hypothesis_index,
+                hypothesis_tokens[alo:ahi]
+            )
+
             diff_class = "table-default"
             if any(
                 token.lower() not in stop_words for token in hypothesis_tokens[alo:ahi]
@@ -132,22 +173,23 @@ def get_diff_score(
                 'type': diff_code_mapping[diff_class],
                 'content': hypothesis_tokens[alo:ahi],
                 'span_hypothesis': [alo, ahi],
-                'span_reference': [blo, bhi]
+                'span_reference': [blo, bhi],
+                'span_spacy': [start, end]
             })
         else:
             if tag == "delete":
-                diff_text, hypothesis_index = get_substring(
-                    hypothesis,
-                    hypothesis_lc,
+                diff_text, hypothesis_index, start, end = get_sublist(
+                    gen_spacy_tokens,
                     hypothesis_index,
-                    hypothesis_tokens[alo:ahi],
+                    hypothesis_tokens[alo:ahi]
                 )
                 # user deleted something, none span in reference
                 diffs_result_list.append({
-                    'type': diff_code_mapping['table-danger'],
+                    'type': diff_code_mapping[diff_class],
                     'content': hypothesis_tokens[alo:ahi],
                     'span_hypothesis': [alo, ahi],
-                    'span_reference': None
+                    'span_reference': [blo, bhi],
+                    'span_spacy': [start, end]
                 })
             elif tag == "insert":
                 diff_text, reference_index = get_substring(
@@ -161,18 +203,18 @@ def get_diff_score(
                     'span_reference': [blo, bhi]
                 })
             elif tag == "replace":
-                diff_text, hypothesis_index = get_substring(
-                    hypothesis,
-                    hypothesis_lc,
+                diff_text, hypothesis_index, start, end = get_sublist(
+                    gen_spacy_tokens,
                     hypothesis_index,
-                    hypothesis_tokens[alo:ahi],
+                    hypothesis_tokens[alo:ahi]
                 )
                 # user deleted something, none span in reference
                 diffs_result_list.append({
-                    'type': diff_code_mapping['table-danger'],
+                    'type': diff_code_mapping[diff_class],
                     'content': hypothesis_tokens[alo:ahi],
                     'span_hypothesis': [alo, ahi],
-                    'span_reference': None
+                    'span_reference': [blo, bhi],
+                    'span_spacy': [start, end]
                 })
                 diff_text, reference_index = get_substring(
                     reference, reference_lc, reference_index, reference_tokens[blo:bhi]
